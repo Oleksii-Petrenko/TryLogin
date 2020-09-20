45a9d9fc052d45a82a0542f3a7244d04f00aa54f
O.Petrenko <petrenko1982w@gmail.com>
2020-09-20 21:33:44 +0300

    Its works ! wow wow wow :)

---
+++ TryLogin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TryLogin.MvcApplication" Language="C#" %>
---
+++ TryLogin/Models/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using TryLogin.ServiceReference1;
+
+namespace TryLogin.Models
+{
+    public class RegisterModel
+    {
+        
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int CountryID { get; set; }
+
+        public int aID { get; set; }
+
+        public string SignupIP { get; set; }
+
+    }
+}
